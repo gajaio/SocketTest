209967071b158e14ab368d02d82ae881ba2724de
Srikanth Modegunta <smodegunta@gmail.com>
2021-01-12 12:36:59 +0530

    added auto response rules support

---
+++ src/main/java/net/sf/sockettest/callbacks/DisconnectListener.java
@@ -0,0 +1,8 @@
+package net.sf.sockettest.callbacks;
+
+/**
+ * Created by emodsri on 3/2/2016.
+ */
+public interface DisconnectListener {
+    void onDisconnect();
+}
---
+++ src/main/java/net/sf/sockettest/callbacks/OnSave.java
@@ -0,0 +1,8 @@
+package net.sf.sockettest.callbacks;
+
+/**
+ * Created by emodsri on 2/24/2016.
+ */
+public interface OnSave<T> {
+    void onSave(T obj);
+}
